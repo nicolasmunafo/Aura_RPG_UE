a37dca74cea51622d60f1ba1c65ce566b293ad67
nicolasmunafo <nicolas.a.munafo@gmail.com>
2023-11-25 01:24:43 +0100

    Player Controller class

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,37 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;		// When an entity changes on the server, the change will replicate or be sent down to the client
+
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);		// Assert that the Aura Context is set
+
+	// To include the Input Mapping Context, we have to access it through the Enhanced Input Local Player Subsystem
+	// Subsystems are singletons
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);	
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// Show mouse cursor with a default cursor
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// Change the input mode of the player controller with configuration such as do not lock mouse to viewport and do not hide cursor during capture
+	FInputModeGameAndUI InputModeData;	// Create an Input Mode Data variable
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);	// Set the input mode of the current player controller to the one created
+
+
+
+	
+
+}
