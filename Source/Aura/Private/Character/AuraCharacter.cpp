fc6bb6df3be701aac8baeb8c2033584c08c90956
nicolasmunafo <nicolas.a.munafo@gmail.com>
2023-11-26 02:03:01 +0100

    Character movement and animation fixes

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,18 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;	// Rotate the character towards the acceleration vector
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);	
+	GetCharacterMovement()->bConstrainToPlane = true;	// Constrain the movement to a plane
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;	// for a Top down game
+
+	// The character itself shouldn't use the controller's rotation
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+}
